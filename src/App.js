ddf191af62922cb17f907913ee7ec8a72b7e8a79
saisrivenkat <50038114+saisrivenkat@users.noreply.github.com>
2021-05-27 10:22:11 +0530

    added localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import Home from './Components/Home'
 
@@ -6,6 +6,12 @@ function App() {
   const [search, setsearch] = useState('')
   const [todo, settodo] = useState([])
   const [dropown, setdropdown] = useState('notcompleted')
+  useEffect(() => {
+    getlocalstorage()
+  }, [])
+  useEffect(() => {
+    savelocalstorage()
+  }, [todo])
 
   const submit = (e) => {
     e.preventDefault()
@@ -29,6 +35,18 @@ function App() {
   const deletetodo = (id) => {
     settodo(todo.filter(item => item.id !== id))
   }
+  const savelocalstorage = () => {
+    localStorage.setItem('todo', JSON.stringify(todo))
+  }
+  const getlocalstorage = () => {
+    if (localStorage.getItem('todo') === null) {
+      localStorage.setItem('todo', JSON.stringify([]));
+    }
+    else {
+      let localcart = JSON.parse(localStorage.getItem('todo'))
+      settodo(localcart)
+    }
+  }
   return (
     <div className="App">
       <Home
@@ -40,8 +58,7 @@ function App() {
         todo={todo}
         submit={submit}
         deletetodo={deletetodo}
-        change={change}
-      />
+        change={change} />
     </div>
   );
 }
